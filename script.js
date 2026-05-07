34ee3728ad8c35029731061ab22b20c43c7acfa6
Ricardo Juliano <rjulianojunior@gmail.com>
2026-05-07 18:21:51 -0300

    adiciona responsividade mobile com menu hamburguer

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ function adicionarAoCarrinho(nomeProduto) {
     document.getElementById('contador-carrinho').textContent = totalItens
     alert(nomeProduto + ' adicionado ao carrinho!')
 }
+function toggleMenu() {
+    const menu = document.querySelector('nav ul')
+    menu.classList.toggle('aberto')
+}
